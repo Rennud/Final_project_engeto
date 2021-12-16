9433df037fc1326a6b3ce253edd55f923eb93e4a
Rennud <martin.sedlak777@gmail.com>
2021-12-17 00:26:56 +0100

    just recreate cuz bug

--- election_main.py
+++ election_main.py
@@ -1,1 +1,15 @@
-    
+import sys
+import save_csv_func
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    save_csv = sys.argv[2]
+    main()
+else:
+    print("RUNNING FILE REQUIRED ENTRY URL AND RUNNING THE FILE REQUIRES THE ENTRY URL AND FILE NAME WHERE YOU WANT "
+          "TO SAVE THE DATA")
