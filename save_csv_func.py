19327fe4c96749bb73e0c5a8dc1dcbf682b349c7
Rennud <martin.sedlak777@gmail.com>
2021-12-23 18:36:30 +0100

    fix parties votes

--- save_csv_func.py
+++ save_csv_func.py
@@ -21,42 +21,17 @@ def create_csv(district_url):
     envelopes = [i[1] for i in vote_data]
     valid = [i[2] for i in vote_data]
     votes_parties = [i[4] for i in vote_data]
-    ods = [i[0] for i in votes_parties]
-    rn_vu = [i[1] for i in votes_parties]
-    cos = [i[2] for i in votes_parties]
-    cssd = [i[3] for i in votes_parties]
-    rc = [i[4] for i in votes_parties]
-    stan = [i[5] for i in votes_parties]
-    komun = [i[6] for i in votes_parties]
-    zel = [i[7] for i in votes_parties]
-    rozum = [i[8] for i in votes_parties]
-    sso = [i[9] for i in votes_parties]
-    bpi = [i[10] for i in votes_parties]
-    oda = [i[11] for i in votes_parties]
-    pirati = [i[12] for i in votes_parties]
-    unie_havel = [i[13] for i in votes_parties]
-    ref_eu = [i[14] for i in votes_parties]
-    top = [i[15] for i in votes_parties]
-    ano = [i[16] for i in votes_parties]
-    dv = [i[17] for i in votes_parties]
-    spr = [i[18] for i in votes_parties]
-    kdu = [i[19] for i in votes_parties]
-    csns = [i[20] for i in votes_parties]
-    realiste = [i[21] for i in votes_parties]
-    sportovci = [i[22] for i in votes_parties]
-    dsss = [i[23] for i in votes_parties]
-    spd = [i[24] for i in votes_parties]
-    spo = [i[25] for i in votes_parties]
+    format_votes = [",".join(i).strip("") for i in votes_parties]
     # I use zip to save the data to csv in the required order
-    rows = zip(code, location, registered, valid, envelopes, ods, rn_vu, cos, cssd,
-               rc, stan, komun, zel, rozum, sso, bpi, oda, pirati, unie_havel, ref_eu,
-               top, ano, dv, spr, kdu, csns, realiste, sportovci, dsss, spd, spo)
+    rows = zip(code, location, registered, valid, envelopes, format_votes)
     # Creating csv file
     header = ["CODE", "LOCATION", "REGISTERED", "ENVELOPES", "VALID"]
     print("SAVING TO FILE: vysledky_kladno.csv")
     with open("vysledky_kladno.csv", "w", newline="", encoding="utf-8") as csv_file:
-        writer = csv.writer(csv_file)
+        writer = csv.writer(csv_file, escapechar=" ", quoting=csv.QUOTE_NONE)
         for name in names:
             header.append(name)
         writer.writerow(header)
         writer.writerows(rows)
+
+create_csv("https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=3&xnumnuts=3101")
